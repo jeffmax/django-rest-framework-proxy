12eb85181572606b3dcf54163176924dce36817f
Jeff Miller <jeffmax@gmail.com>
2015-10-07 14:48:18 -0400

    Update views.py

--- rest_framework_proxy/views.py
+++ rest_framework_proxy/views.py
@@ -130,7 +130,7 @@ class ProxyView(BaseProxyView):
 
     def create_response(self, response):
         if self.return_raw or self.proxy_settings.RETURN_RAW:
-            r = HttpResponse(response.raw.read())
+            r = HttpResponse(response.content)
             for key,item in response.headers.items():
                 r[key] = item
             return r
